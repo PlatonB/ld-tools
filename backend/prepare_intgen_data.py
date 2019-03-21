3c41b315b46cdcb0321adb3bced254b69f4f1bc5
PlatonB <platon.work@gmail.com>
2019-03-21 11:59:57 +0300

    Модуль подготовки данных 1000 Genomes, V2.0
    - У баз теперь будет расширение .dbm. Перед запуском LD_triangle версии 3.8 или старше удалите, пожалуйста, созданные ранее базы.
    - Теперь станут возвращаться пути не к архивам и базам по-отдельности, а к общим названиям (без расширений) для каждой пары архив-база.
    - Переименовывание многих переменных.

--- backend/prepare_intgen_data.py
+++ backend/prepare_intgen_data.py
@@ -1,39 +1,40 @@
-__version__ = 'V1.2'
+__version__ = 'V2.0'
 
 import urllib.request, re, os, json, gzip, sys, dbm
 
-def download_intgen_data(intgen_ftp_url, intgen_data_name, intgen_dir_path, index):
+def download_intgen_file(intgen_ftp_url, intgen_file_name, intgen_dir_path, index):
         '''
         Скачивание данных 1000 Genomes.
         '''
         
-        #Формирование ссылки на скачивание файла
-        #1000 Genomes из официального FTP этого проекта,
-        #абсолютного пути к этому файлу на компьютере пользователя
-        #и часть имени файла без расширения(-ий) (далее - название).
-        intgen_data_url = os.path.join(intgen_ftp_url, intgen_data_name)
-        intgen_data_path = os.path.join(intgen_dir_path, intgen_data_name)
-        intgen_data_base = '.'.join(intgen_data_name.split('.')[:index])
+        #Формирование ссылки на скачивание файла 1000
+        #Genomes из официального FTP этого проекта, абсолютного
+        #пути к этому файлу на компьютере пользователя и часть
+        #имени файла без расширения(-ий) (далее - название).
+        intgen_file_url = os.path.join(intgen_ftp_url, intgen_file_name)
+        intgen_file_path = os.path.join(intgen_dir_path, intgen_file_name)
+        intgen_filebase_name = '.'.join(intgen_file_name.split('.')[:index])
         
         #Если 1000 Genomes-файл ещё не скачан, то
         #будут производиться попытки это сделать.
         #В случае, если скачивание срывается из-за
-        #сбоев серверов IGSR, эта функция будет рекурсивно
-        #запускаться ещё и ещё раз до достижения успеха.
+        #сбоев серверов IGSR, эта функция
+        #будет рекурсивно запускаться ещё
+        #и ещё раз до достижения успеха.
         #После скачивания, или если файл
         #уже есть на диске, функция вернёт
         #путь к нему, а также его название.
-        if os.path.exists(intgen_data_path) == False:
-                print(f'{intgen_data_name} не найден. Загрузка...')
+        if os.path.exists(intgen_file_path) == False:
+                print(f'{intgen_file_name} не найден. Загрузка...')
                 try:
-                        urllib.request.urlretrieve(intgen_data_url, intgen_data_path)
-                        return intgen_data_path, intgen_data_base
+                        urllib.request.urlretrieve(intgen_file_url, intgen_file_path)
+                        return intgen_file_path, intgen_filebase_name
                 except urllib.error.URLError:
                         print('Сбой соединения. Повторная попытка:')
-                        return download_intgen_data(intgen_ftp_url, intgen_data_name, intgen_dir_path, index)
+                        return download_intgen_file(intgen_ftp_url, intgen_file_name, intgen_dir_path, index)
         else:
-                print(f'{intgen_data_name} уже скачан')
-                return intgen_data_path, intgen_data_base
+                print(f'{intgen_file_name} уже скачан')
+                return intgen_file_path, intgen_filebase_name
         
 '''
 Отделение от строки символа переноса, её преобразование
@@ -47,8 +48,11 @@ compress_line = lambda line: gzip.compress(line.split('\n')[0].encode())
 
 def process_intgen_data(intgen_dir_path):
         '''
-        Размещение данных 1000 Genomes в dbm-базы, из которых можно
-        будет быстро извлекать строки по идентификаторам SNP.
+        Формирование JSON-редакции панели сэмплов.
+        Размещение данных 1000 Genomes по
+        SNPs в dbm-базы, из которых можно
+        будет быстро извлекать строки
+        по идентификаторам SNP.
         '''
         
         #Если папка для 1000 Genomes-данных и
@@ -76,10 +80,11 @@ def process_intgen_data(intgen_dir_path):
         #Если сохранённой панели нет, скачиваем её.
         #Получаем путь к панели и название.
         print('')
-        intgen_samptxt_path, intgen_samptxt_base = download_intgen_data(intgen_ftp_url, intgen_samptxt_name, intgen_dir_path, -1)
+        intgen_samptxt_path, intgen_sampbase_name = download_intgen_file(intgen_ftp_url, intgen_samptxt_name, intgen_dir_path, -1)
         
-        #Собираем имя будущей JSON-редакции панели и путь к ней.
-        intgen_sampjson_name = intgen_samptxt_base + '.json'
+        #Собираем из названия и расширения имя
+        #будущей JSON-редакции панели и путь к ней.
+        intgen_sampjson_name = intgen_sampbase_name + '.json'
         intgen_sampjson_path = os.path.join(intgen_dir_path, intgen_sampjson_name)
         
         #JSON-панель создастся только, если она ещё не создана.
@@ -129,55 +134,55 @@ def process_intgen_data(intgen_dir_path):
         #В каждом архиве - VCF-таблица с данными по SNPs
         #одной хромосомы: идентификатор снипа, некоторые
         #его характеристики и набор фазированных генотипов.
-        intgen_natgz_names = re.findall(r'ALL\.chr[^(?:MT)]\S+?\.vcf\.gz(?=\\)', intgen_ftp_page)
+        intgen_vcfgz_names = re.findall(r'ALL\.chr[^(?:MT)]\S+?\.vcf\.gz(?=\\)', intgen_ftp_page)
         
-        #Списки для накопления путей к архивам
-        #1000 Genomes и производным базам.
-        intgen_natgz_paths, intgen_natdb_paths = [], []
+        #Списки для накопления путей к
+        #файлам 1000 Genomes без расширений.
+        #Расширения будут добавляться
+        #уже на уровне фронтендов.
+        intgen_vcfbase_paths = []
         
         #В рамках этого цикла будут создаваться и пополняться
         #dbm-базы, основанные на данных из 1000 Genomes-VCFs.
-        for intgen_natgz_name in intgen_natgz_names:
-                
-                #Проверка на наличие табикс-индексов.
-                #Если их нет - они скачаются.
-                #Сохранять возвращаемые функцией
-                #пути к tbi-файлам не нужно, т.к.
-                #pysam находит индексы самостоятельно.
-                download_intgen_data(intgen_ftp_url, intgen_natgz_name + '.tbi', intgen_dir_path, -3)
-                
-                #При обнаружении отсутствия архива 1000
-                #Genomes вызываемая функция скачает его, а
-                #также возвратит путь к архиву и его название.
+        #Попутно будут скачиваться tabix-индексы.
+        for intgen_vcfgz_name in intgen_vcfgz_names:
+
+                #Формирование имени табикс-индекса.
+                #При обнаружениии отсутствия этого
+                #файла вызываемая функция скачает его.
+                intgen_vcftbi_name = intgen_vcfgz_name + '.tbi'
+                download_intgen_file(intgen_ftp_url, intgen_vcftbi_name, intgen_dir_path, -3)
+
+                #Проверка на наличие архива 1000 Genomes.
+                #Если его нет - он скачается, а также будет
+                #возвращён путь к архиву без расширения.
                 #Путь будет добавлен в список таких путей.
-                intgen_natgz_path, intgen_natgz_base = download_intgen_data(intgen_ftp_url, intgen_natgz_name, intgen_dir_path, -2)
-                intgen_natgz_paths.append(intgen_natgz_path)
+                intgen_vcfgz_path, intgen_vcfbase_name = download_intgen_file(intgen_ftp_url, intgen_vcfgz_name, intgen_dir_path, -2)
+                intgen_vcfbase_paths.append(os.path.join(intgen_dir_path, intgen_vcfbase_name))
                 
                 #Конструирование имени и пути для далее создаваемой базы.
-                #Добавление пути в соответствующий список.
-                intgen_natdb_name = intgen_natgz_base + '_dbm'
-                intgen_natdb_path = os.path.join(intgen_dir_path, intgen_natdb_name)
-                intgen_natdb_paths.append(intgen_natdb_path)
+                intgen_vcfdb_name = intgen_vcfbase_name + '.dbm'
+                intgen_vcfdb_path = os.path.join(intgen_dir_path, intgen_vcfdb_name)
                 
                 #Проверка, нет ли у пользователя уже готовой базы.
                 #Если есть, то пропускаем процесс создания.
-                if os.path.exists(intgen_natdb_path) == True:
-                        print(f'{intgen_natdb_name} уже создан\n')
+                if os.path.exists(intgen_vcfdb_path) == True:
+                        print(f'{intgen_vcfdb_name} уже создан\n')
                         continue
-                print(f'{intgen_natdb_name} не найден. Создание...\n')
+                print(f'{intgen_vcfdb_name} не найден. Создание...\n')
                 
                 #Открываем архив 1000 Genomes на чтение таким образом, чтобы 
                 #находящийся внутри VCF считывался как строковые данные.
-                with gzip.open(intgen_natgz_path, mode='rt') as intgen_natgz_opened:
+                with gzip.open(intgen_vcfgz_path, mode='rt') as intgen_vcfgz_opened:
                         
                         #Создание dbm-базы.
-                        with dbm.open(intgen_natdb_path, 'c') as intgen_natdb_opened:
+                        with dbm.open(intgen_vcfdb_path, 'c') as intgen_vcfdb_opened:
                                 
                                 #Прочтение вхолостую строк метаинформации.
                                 #Получение шапки VCF-таблицы.
-                                meta_line = intgen_natgz_opened.readline()
+                                meta_line = intgen_vcfgz_opened.readline()
                                 while meta_line.startswith('##'):
-                                        meta_line = intgen_natgz_opened.readline()
+                                        meta_line = intgen_vcfgz_opened.readline()
                                 header_line = meta_line
                                 
                                 #Валидация первых 9 элементов шапки.
@@ -190,16 +195,16 @@ def process_intgen_data(intgen_dir_path):
                                                                    'FILTER',
                                                                    'INFO',
                                                                    'FORMAT']:
-                                        print(f'''{intgen_natgz_name}, скорее всего, теперь другой структуры.
+                                        print(f'''{intgen_vcfgz_name}, скорее всего, теперь другой структуры.
 Пожалуйста, сообщите об этом в Issues.
 Работа программы прервана.''')
                                         sys.exit()
                                         
                                 #Добавление шапки в базу.
-                                intgen_natdb_opened['header_line'] = compress_line(header_line)
+                                intgen_vcfdb_opened['header_line'] = compress_line(header_line)
                                 
                                 #Построчное прочтение основной части VCF-таблицы.
-                                for line in intgen_natgz_opened:
+                                for line in intgen_vcfgz_opened:
                                         
                                         #Создание списка из идентификатора SNP,
                                         #референсного и альтернативного(-ых) аллеля(-ей).
@@ -223,8 +228,6 @@ def process_intgen_data(intgen_dir_path):
                                         #поэтому соответствующие refSNPID
                                         #в базу добавляться не будут.
                                         if re.match(r'rs\d+$', rs_cell) != None and alt_cell.find(',') == -1:
-                                                intgen_natdb_opened[rs_cell] = compress_line(line)
+                                                intgen_vcfdb_opened[rs_cell] = compress_line(line)
                                                 
-        return intgen_sampjson_path, intgen_natgz_paths, intgen_natdb_paths
-
-#intgen_sampjson_path, intgen_natgz_paths, intgen_natdb_paths = process_intgen_data('/home/platon/_0_Диссертация/Exp/1000G/data')
+        return intgen_sampjson_path, intgen_vcfbase_paths
