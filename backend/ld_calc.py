23a9a9e8d619e899840688e1a5718dda6331ffc8
PlatonB <platon.work@gmail.com>
2019-04-14 20:56:08 +0300

    Модуль вычисления LD, V3.0
    - Помимо, собственно, значений LD модуль будет возвращать частоты альтернативных аллелей.
    - Переработка имён переменных с целью внесения ясности, какие значения относятся к референсному аллелю, а какие - к альтернативному.

--- backend/ld_calc.py
+++ backend/ld_calc.py
@@ -1,4 +1,4 @@
-__version__ = 'V2.0'
+__version__ = 'V3.0'
 
 import os, json
 
@@ -16,9 +16,9 @@ def ld_calc(snp_1_row, snp_2_row, sample_indices):
         #Фазированные генотипы бывают такими: 1|1, 1|0, 0|1, 0|0, 1, 0.
         #То, что в каждом, если не рассматривать X и Y-хромосомы,
         #два значения - следствие диплоидности у человека.
-        #К примеру, 0|1 показывает, что в данной позиции
-        #у этого индивида на одной копии хромосомы -
-        #референсный аллель, а на другой - альтернативный.
+        #К примеру, 0|1 показывает, что в данной позиции у
+        #этого индивида на одной копии хромосомы - референсный
+        #аллель (0), а на другой - альтернативный (1).
         snp_1_phased_genotypes = [snp_1_row[sample_index] for sample_index in sample_indices]
         snp_2_phased_genotypes = [snp_2_row[sample_index] for sample_index in sample_indices]
         
@@ -33,38 +33,38 @@ def ld_calc(snp_1_row, snp_2_row, sample_indices):
         #Оно равно общему количеству отдельных генотипов.
         #Это значение константно в пределах
         #каждой таблицы 1000 Gemomes.
-        haplotype_quan = len(snp_1_genotypes)
+        htype_quan = len(snp_1_genotypes)
         
         #Рассчёт количества каждого гаплотипа и каждого аллеля.
-        haplotype_11_quan, haplotype_12_quan, haplotype_21_quan, haplotype_22_quan = 0, 0, 0, 0
-        a1_allele_quan, a2_allele_quan, b1_allele_quan, b2_allele_quan = 0, 0, 0, 0
-        for haplotype in zip(snp_1_genotypes, snp_2_genotypes):
-                if haplotype[0] == '1' and haplotype[1] == '1':
-                        haplotype_11_quan += 1
-                        a1_allele_quan += 1
-                        b1_allele_quan += 1
-                elif haplotype[0] == '1' and haplotype[1] == '0':
-                        haplotype_12_quan += 1
-                        a1_allele_quan += 1
-                        b2_allele_quan += 1
-                elif haplotype[0] == '0' and haplotype[1] == '1':
-                        haplotype_21_quan += 1
-                        a2_allele_quan += 1
-                        b1_allele_quan += 1
-                elif haplotype[0] == '0' and haplotype[1] == '0':
-                        haplotype_22_quan += 1
-                        a2_allele_quan += 1
-                        b2_allele_quan += 1
+        htype_alt_alt_quan, htype_alt_ref_quan, htype_ref_alt_quan, htype_ref_ref_quan = 0, 0, 0, 0
+        snp_1_alt_quan, snp_1_ref_quan, snp_2_alt_quan, snp_2_ref_quan = 0, 0, 0, 0
+        for htype in zip(snp_1_genotypes, snp_2_genotypes):
+                if htype[0] == '1' and htype[1] == '1':
+                        htype_alt_alt_quan += 1
+                        snp_1_alt_quan += 1
+                        snp_2_alt_quan += 1
+                elif htype[0] == '1' and htype[1] == '0':
+                        htype_alt_ref_quan += 1
+                        snp_1_alt_quan += 1
+                        snp_2_ref_quan += 1
+                elif htype[0] == '0' and htype[1] == '1':
+                        htype_ref_alt_quan += 1
+                        snp_1_ref_quan += 1
+                        snp_2_alt_quan += 1
+                elif htype[0] == '0' and htype[1] == '0':
+                        htype_ref_ref_quan += 1
+                        snp_1_ref_quan += 1
+                        snp_2_ref_quan += 1
                         
         #Рассчёт частоты каждого гаплотипа и каждого аллеля.
-        x11 = haplotype_11_quan / haplotype_quan
-        x12 = haplotype_12_quan / haplotype_quan
-        x21 = haplotype_21_quan / haplotype_quan
-        x22 = haplotype_22_quan / haplotype_quan
-        p1 = a1_allele_quan / haplotype_quan
-        p2 = a2_allele_quan / haplotype_quan
-        q1 = b1_allele_quan / haplotype_quan
-        q2 = b2_allele_quan / haplotype_quan
+        alt_alt_htype_freq = htype_alt_alt_quan / htype_quan
+        alt_ref_htype_freq = htype_alt_ref_quan / htype_quan
+        ref_alt_htype_freq = htype_ref_alt_quan / htype_quan
+        ref_ref_htype_freq = htype_ref_ref_quan / htype_quan
+        snp_1_alt_freq = snp_1_alt_quan / htype_quan
+        snp_1_ref_freq = snp_1_ref_quan / htype_quan
+        snp_2_alt_freq = snp_2_alt_quan / htype_quan
+        snp_2_ref_freq = snp_2_ref_quan / htype_quan
         
         #D - стандартное значение LD.
         #Если оно равно нулю, то это
@@ -72,7 +72,7 @@ def ld_calc(snp_1_row, snp_2_row, sample_indices):
         #Неравенство нулю будет говорить
         #о неравновесии по сцеплению.
         #D может быть от -0.25 до 0.25.
-        d = x11 - p1 * q1
+        d = alt_alt_htype_freq - snp_1_alt_freq * snp_2_alt_freq
         
         #D' - нормализованное значение LD.
         #Частота одного из аллелей в редких
@@ -86,13 +86,13 @@ def ld_calc(snp_1_row, snp_2_row, sample_indices):
         #проблемы на уровне фронтендов, при обработке
         #мономорфных SNP будет возвращён D', равный 0.
         if d >= 0:
-                d_max = min(p1 * q2, p2 * q1)
+                d_max = min(snp_1_alt_freq * snp_2_ref_freq, snp_1_ref_freq * snp_2_alt_freq)
                 try:
                         d_prime = round(d / d_max, 5)
                 except ZeroDivisionError:
                         d_prime = 0
         else:
-                d_min = max(-p1 * q1, -p2 * q2)
+                d_min = max(-snp_1_alt_freq * snp_2_alt_freq, -snp_1_ref_freq * snp_2_ref_freq)
                 try:
                         d_prime = round(d / d_min, 5)
                 except ZeroDivisionError:
@@ -106,10 +106,15 @@ def ld_calc(snp_1_row, snp_2_row, sample_indices):
         #в качестве значения r2 возвратится 0.
         #Эта ситуация подробно описана выше для D'.
         if d_prime != 0:
-                r_square = round((d ** 2) / (p1 * p2 * q1 * q2), 5)
+                r_square = round((d ** 2) / (snp_1_alt_freq * snp_1_ref_freq * snp_2_alt_freq * snp_2_ref_freq), 5)
         else:
                 r_square = 0
 
-        #Оба значения LD организуем в словарь и возвращаем.
-        ld_vals = {'r_square': r_square, 'd_prime': d_prime}
-        return ld_vals
+        #Оба значения LD, а также
+        #частоты альтернативных аллелей,
+        #организуем в словарь и возвращаем.
+        results = {'r_square': r_square,
+                   'd_prime': d_prime,
+                   'snp_1_alt_freq': round(snp_1_alt_freq, 5),
+                   'snp_2_alt_freq': round(snp_2_alt_freq, 5)}
+        return results
